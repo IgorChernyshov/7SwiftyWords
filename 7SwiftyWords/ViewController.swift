d4f4dd335b6013f45d405eb3e49e427680e3f6ac
Igor Chernyshov <Chernyshov.I.Ser@sberbank.ru>
2021-06-28 08:13:20 +0300

    Initial Commit

---
+++ 7SwiftyWords/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7SwiftyWords
+//
+//  Created by Igor Chernyshov on 28.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
